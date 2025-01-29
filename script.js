93737a1fa5188222512a2f44d8c08b427f7c1bb9
IshaqJunejo <ishaquejunejo33@gmail.com>
2025-01-29 22:20:45 +0500

    Few Changes in Color Palette and Sections

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 navigation = document.querySelector("nav");
 navItems = document.querySelectorAll(".nav-links");
 
-function NavBar()
-{
+function NavBar() {
     navItems.forEach(item => {
-        if (item.style.display === "inline-block")
-        {
-            setTimeout(() =>
-            {
+        if (item.style.display === "inline-block") {
+            setTimeout(() => {
                 item.style.display = "none";
                 navigation.style.height = "inherit";
                 navigation.querySelector("ul").style.height = "inherit";
                 navigation.querySelector("ul").style.width = "75%";
                 navigation.querySelector(".logo").style.display = "block";
             }, 100);
-        }else
-        {
-            setTimeout(() =>
-            {
+        }else {
+            setTimeout(() => {
                 item.style.display = "inline-block";
                 navigation.style.height = "100vh";
                 navigation.querySelector("ul").style.height = "inherit";
@@ -28,13 +23,10 @@ function NavBar()
     });
 }
 
-function CloseNav()
-{
+function CloseNav() {
     navItems.forEach(item => {
-        if (item.style.display === "inline-block")
-        {
-            setTimeout(() =>
-            {
+        if (item.style.display === "inline-block") {
+            setTimeout(() => {
                 item.style.display = "none";
                 navigation.style.height = "inherit";
                 navigation.querySelector("ul").style.height = "inherit";
@@ -47,11 +39,11 @@ function CloseNav()
 
 window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
-    if (scroll > 0)
-    {
-        navigation.style.setProperty("filter", "drop-shadow(0px 6px 3px #2d2d30)");
-    }else
-    {
-        navigation.style.setProperty("filter", "drop-shadow(0px 0px 0px #2d2d30)");
+    if (scroll > 2) {
+        //navigation.style.setProperty("filter", "drop-shadow(0px 6px 3px #2d2d30)");
+        navigation.style.setProperty("border-bottom", "#3A3F47 2px solid");
+    }else {
+        //navigation.style.setProperty("filter", "drop-shadow(0px 0px 0px #2d2d30)");
+        navigation.style.setProperty("border-bottom", "none");
     }
 });
